eba651249da8fbdf257ada1d701ba74ac2940b4d
tinapayshakera <tinapayshakera@gmail.com>
2022-09-08 17:01:33 +0800

    collab.java

--- Collab.java
+++ Collab.java
@@ -2,7 +2,7 @@ public class Collab {
     public static void main(String args[]) {
       
       System.out.println("Shakera Joy Tinapay");
-	System.out.println("Lesly J. Bataluna");
+	
 
     }
 }
