8ba50499bdd99b795e5e37651712ffcf8e15c329
tinapayshakera <tinapayshakera@gmail.com>
2022-09-15 10:31:14 +0800

    subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,9 @@
+public class subtraction {
+    public static void main(String args[]) {
+      int a=20;
+      int b=40;
+      int c=a-b;
+
+      System.out.println("The answer of a-b is = " + c );
+    }
+}
