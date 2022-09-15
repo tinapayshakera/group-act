80fdf912bc5ffba159ee97270a00e73e83c3551b
tinapayshakera <tinapayshakera@gmail.com>
2022-09-15 08:45:47 +0800

    Colab.java

---
+++ Colab.java
@@ -0,0 +1,10 @@
+public class Colab {
+    public static void main(String args[]) {
+      
+      int a=20;
+      int b=40;
+      int c=a-b;
+
+      System.out.println("The answer of a-b is = " + c );
+    }
+}
