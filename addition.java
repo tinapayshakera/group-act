e4bb712a5efff4a0a72bc04c05192ec8c16647db
slyiin <batalunaleslyj@gmail.com>
2022-09-15 08:24:44 +0800

    added addition

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class Collab {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 5;
+        int c = a+b;
+
+        System.out.println("a+b=" + c);
+    }
+}
