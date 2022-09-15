268c564dc8611c0346c89dfc6acca187a272cee3
tinapayshakera <tinapayshakera@gmail.com>
2022-09-15 20:04:52 +0800

    merge feature/multiplication

--- multiplication.java
+++ multiplication.java
@@ -1,11 +1,12 @@
 public class multiplication{
-public static void main(String args[]){
+    public static void main(String args[]){
 
 
-int x = 34;
-int y = 16;
-int z = x * y;
-System.out.println("Multiplication: " + z);
+        int x = 34;
+        int y = 16;
+        int z = x * y;
+
+    System.out.println("Multiplication: " + z);
     
     }
     
