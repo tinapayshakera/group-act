eb450a927b003b2d9e0db32b8093d0a8232499de
tinapayshakera <tinapayshakera@gmail.com>
2022-09-16 17:56:00 +0800

    division.java

---
+++ division.java
@@ -0,0 +1,19 @@
+
+
+
+public class division{
+    public static void main(String args[]){
+   
+    int num1= 23;
+    int num2= 5;
+    int ans= num1/num2;
+
+    System.out.println("Division:" + ans);
+    }
+}
+    
+
+
+
+
+
