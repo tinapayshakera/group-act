350231e01194bc2fd39ec567462464ebd4c2c45f
slyiin <batalunaleslyj@gmail.com>
2022-09-16 21:53:34 +0800

    added multiplication

--- Calculation.java
+++ Calculation.java
@@ -23,7 +23,15 @@ public class Calculation{
     }
 }
 
+public class Calculation {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        int c = a*b;
 
+        System.out.println("a*b" + c);
+    }
+}
 
 
 
