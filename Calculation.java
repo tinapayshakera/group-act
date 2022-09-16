72bd0263e0a1ffa8bad22858b26c594bdbcbc07b
slyiin <batalunaleslyj@gmail.com>
2022-09-16 21:48:11 +0800

    added division

--- Calculation.java
+++ Calculation.java
@@ -7,3 +7,19 @@ public class Calculation {
         System.out.println("a+b=" + c);
     }
 }
+
+
+
+
+
+
+public class Calculation {
+    public static void main(String args[]) {
+        int a = 12;
+        int b = 3;
+        int c = a/b;
+
+        System.out.println("a/b=" + c)
+        
+    }
+}
