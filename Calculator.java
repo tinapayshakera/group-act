a8591b4a6a2c0d4b38e2495e643abf085d2b8566
tinapayshakera <tinapayshakera@gmail.com>
2022-09-16 20:14:45 +0800

    delete

--- Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public static void main(String args[]) {
-        int a = 5;
-        int b = 5;
-        int c = a+b;
-
-        System.out.println("a+b=" + c);
-    }
-}
