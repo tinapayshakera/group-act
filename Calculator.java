3bbb855333bc236a3b3b2a790ea7cd2b36ce5bb2
tinapayshakera <tinapayshakera@gmail.com>
2022-09-16 21:28:43 +0800

    added

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
     public static void main(String args[]) {
         int a = 5;
-        int b = 5;
+        int b = 18;
         int c = a+b;
 
         System.out.println("a+b=" + c);
