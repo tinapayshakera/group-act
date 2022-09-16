bcec1e4b5b64858a5f0eda3d7f4a24ef72dda316
tinapayshakera <tinapayshakera@gmail.com>
2022-09-16 21:42:56 +0800

    merge to develop

--- Calculation.java
+++ Calculation.java
@@ -7,6 +7,13 @@ public class Calculation {
 
         System.out.println("a+b=" + c);
 =======
+
+public class Calculation{
+
+    public static void main(String args[]){
+
+    
+
       int a=20;
       int b=30;
       int c=a-b;
