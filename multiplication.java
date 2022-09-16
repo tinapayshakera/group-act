20ded42bdb3c22596d6cbebd20e5fc1319865f7b
tinapayshakera <tinapayshakera@gmail.com>
2022-09-16 16:33:27 +0800

    multiplication.java

--- multiplication.java
+++ multiplication.java
@@ -2,8 +2,8 @@ public class multiplication{
     public static void main(String args[]){
 
 
-        int x = 27;
-        int y = 34;
+        int x = 17;
+        int y = 37;
         int z = x * y;
     System.out.println("Multiplication: " + z);
     
